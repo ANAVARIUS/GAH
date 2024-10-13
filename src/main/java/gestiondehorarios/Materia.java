0e9e07848e392cc2a7a35707931dc5d7e3154688
Diego <d03.nov@gmail.com>
2024-10-12 20:24:22 -0600

    New organization and creation of database files and methods in the main class

---
+++ src/main/java/gestiondehorarios/Materia.java
@@ -0,0 +1,58 @@
+package gestiondehorarios;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Materia {
+    private String nombreMateria;
+    private int noCreditos;
+    private List<Materia> requisitos = new ArrayList<Materia>();
+
+    public Materia(String nombreMateria, int noCreditos) {
+        this.nombreMateria = nombreMateria;
+        this.noCreditos = noCreditos;
+    }
+
+    public String getNombreMateria() {
+        return nombreMateria;
+    }
+
+    public void setNombreMateria(String nombreMateria) {
+        this.nombreMateria = nombreMateria;
+    }
+
+    public int getNoCreditos() {
+        return noCreditos;
+    }
+
+    public void setNoCreditos(int noCreditos) {
+        this.noCreditos = noCreditos;
+    }
+    public void addRequisitos(Materia materia){
+        if (!requisitos.contains(materia)) {
+            requisitos.add(materia);
+        }
+        else {
+            System.out.println("La materia ya está en la lista de requisitos");
+        }
+    }
+    public void removeRequisitos(Materia materia){
+        if(!requisitos.remove(materia)){
+            System.out.println("Error, la materia se encuentra en los requisitos.");
+        }
+    };
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true; // Mismo objeto
+        if (obj == null || getClass() != obj.getClass()) return false; // Diferente clase
+        Materia materia = (Materia) obj; // Convertir a Materia
+        return noCreditos == materia.noCreditos && nombreMateria.equals(materia.nombreMateria); // Comparar atributos
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombreMateria, noCreditos); // Generar un hash basado en los atributos
+    }
+
+}
